e82bfbdc214ebca79c4cfa90a010f13bf5c45f44
letnaturebe <letnaturebe2@gmail.com>
2025-02-20 10:59:48 +0900

    Update app home opened callback to include API key configuration message and button

--- listeners/events/app-home-opened.ts
+++ listeners/events/app-home-opened.ts
@@ -1,39 +1,68 @@
-import type { AllMiddlewareArgs, SlackEventMiddlewareArgs } from '@slack/bolt';
+import type {AllMiddlewareArgs, SlackEventMiddlewareArgs} from '@slack/bolt';
+import {AnyBlock} from '@slack/types';
 
 const appHomeOpenedCallback = async ({
-  client,
-  event,
-  logger,
-}: AllMiddlewareArgs & SlackEventMiddlewareArgs<'app_home_opened'>) => {
-  // Ignore the `app_home_opened` event for anything but the Home tab
-  if (event.tab !== 'home') return;
+                                         client,
+                                         event,
+                                         logger,
+                                     }: AllMiddlewareArgs & SlackEventMiddlewareArgs<'app_home_opened'>) => {
+    // Ignore the `app_home_opened` event for anything but the Home tab
+    if (event.tab !== 'home') return;
 
-  try {
-    await client.views.publish({
-      user_id: event.user,
-      view: {
-        type: 'home',
-        blocks: [
-          {
+    const message = "To enable this app in this Slack workspace, you need to save your OpenAI API key. " +
+        "Visit <https://platform.openai.com/account/api-keys|your developer page> to grap your key!"
+
+    const blocks: AnyBlock[] = [
+        {
             type: 'section',
             text: {
-              type: 'mrkdwn',
-              text: `*Welcome home, <@${event.user}> :house:*`,
+                type: 'mrkdwn',
+                text: `*Configuration* `,
             },
-          },
-          {
+        },
+        {
+            type: 'divider',
+        },
+        {
             type: 'section',
             text: {
-              type: 'mrkdwn',
-              text: 'Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.',
+                type: 'mrkdwn',
+                text: message || ' ',
             },
-          },
-        ],
-      },
-    });
-  } catch (error) {
-    logger.error(error);
-  }
+            accessory: {
+                action_id: 'configure',
+                type: 'button',
+                text: {
+                    type: 'plain_text',
+                    text: 'Configure',
+                },
+                style: 'primary',
+                value: 'api_key',
+            },
+        },
+        // {
+        //     type: 'section',
+        //     text: {
+        //         type: 'mrkdwn',
+        //         text: `*Welcome home, <@${event.user}> :house:*`,
+        //     },
+        // },
+    ];
+
+    try {
+        await client.views.publish({
+            user_id: event.user,
+            view: {
+                type: 'home',
+                blocks: blocks,
+            },
+        });
+    } catch (error) {
+        logger.error(error);
+    }
 };
 
 export default appHomeOpenedCallback;
+
+
+
